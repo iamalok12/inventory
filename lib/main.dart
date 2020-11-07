7022595214b717b50cf0a65b80b80ea5e3fee172
Alok Kumar <66058732+iamalok12@users.noreply.github.com>
2020-11-07 17:31:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StockDetail(),
+      home: MainScreen(),
       routes: {
         'MainScreen': (context) => MainScreen(),
         'StockDetails': (context) => StockEntry()
